3de19fd93a1c4c8156039cafa07c9cd45187e3c8
tsaiiuo <tsaiiuo1@gmail.com>
2024-11-24 19:48:13 +0800

    test:

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden z-0">
       <Head>
-        <title>Iuo's Portfolio</title>
+        <title>Ian's Portfolio</title>
       </Head>
       {/* Header */}
       <Header />
